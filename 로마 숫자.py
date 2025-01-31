fed650da964b55313d4e6a95a62e860cac21f1b5
Factories-git <official.factories@gmail.com>
2025-01-31 11:36:15 +0900

    Changes

--- 로마 숫자.py
+++ 로마 숫자.py
@@ -21,7 +21,6 @@ for i in range(len(n1)-1):
             pass_1.add(i+1)
         else:
             real_n1 += n1[i]
-
 for i in range(len(n2)-1):
     if i not in pass_2:
         if n2[i] < n2[i+1]:
@@ -40,3 +39,16 @@ str_bout = ''
 for idx, i in enumerate(str(int_bout)):
     str_bout += (i + (len(str(int_bout)) - idx - 1) * '0')
     str_bout += ' '
+num_to_rome = {
+    '3000': 'MMM', '2000': 'MM', '1000': 'M',
+    '900': 'CM', '800': 'DCCC', '700': 'DCC', '600': 'DC', '500': 'D', '400': 'CD',
+    '300': 'CCC', '200': 'CC', '100': 'C',
+    '90': 'XC', '80': 'LXXX', '70': 'LXX', '60': 'LX', '50': 'L', '40': 'XL',
+    '30': 'XXX', '20': 'XX', '10': 'X',
+    '9': 'IX', '8': 'VIII', '7': 'VII', '6': 'VI', '5': 'V', '4': 'IV',
+    '3': 'III', '2': 'II', '1': 'I'
+}
+for n, e in num_to_rome.items():
+    if n in str_bout:
+        str_bout = str_bout.replace(n, e)
+print(''.join(str_bout.split()))
